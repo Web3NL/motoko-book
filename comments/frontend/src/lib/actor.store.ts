81d415969255a095826bc6cc349f00f86ebfefad
Web3NL <samerweb3@gmail.com>
2023-05-29 13:36:17 +0200

    wip

---
+++ comments/frontend/src/lib/actor.store.ts
@@ -0,0 +1,12 @@
+import { HttpAgent, Actor } from '@dfinity/agent';
+import type { Identity } from '@dfinity/agent';
+import { idlFactory } from '../../../declarations/comments.did.js';
+import type { _SERVICE } from '../../../declarations/comments.did.js';
+import { authStore } from './auth.store';
+import { get } from 'svelte/store';
+
+export const canisterId = import.meta.env.VITE_BACKEND_CANISTER_ID;
+
+const id : Identity = get(authStore).identity | undefined;
+const agent = new HttpAgent({id});
+
