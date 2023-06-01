1b3acfd23c64cabd3ab349df03655965f9381662
Web3NL <samerweb3@gmail.com>
2023-06-01 14:53:02 +0200

    minified motoko.js

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   build: {
-    lib: {
-      entry: "script/motoko.js",
-      name: "motoko-book",
-      fileName: "motoko",
-      formats: ["es"],
+    rollupOptions: {
+      input: "script/motoko.js",
+      output: {
+        manualChunks: false,
+        dir: "theme",
+        entryFileNames: "motoko.js",
+        inlineDynamicImports: true,
+      }
     },
-    outDir: "theme",
     emptyOutDir: false,
   },
 })
