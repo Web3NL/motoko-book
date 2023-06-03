65aa74402e9e53dbf16293fabe4b3ee8481dc883
Web3NL <samerweb3@gmail.com>
2023-06-03 20:35:15 +0200

    wip

--- comments/frontend/src/lib/Comment.svelte
+++ comments/frontend/src/lib/Comment.svelte
@@ -2,6 +2,7 @@
 	import type { QueryComment } from '../declarations/comments.did';
 	import type { CommentHash } from '../declarations/comments.did';
 	import { authStore } from '$lib/auth.store';
+	import Button from '$lib/Button.svelte';
 
 	export let queryComment: QueryComment;
 
@@ -13,19 +14,35 @@
 </script>
 
 <div class="card mt-4">
-	<header class="card-header">
-		{created} by {queryComment.userId}
-		Comment Reward: {queryComment.reward}
-	</header>
-	<section class="p-4">{queryComment.comment}</section>
-	<footer class="card-footer">
+	<div class="flex justify-between">
+		<p class="p-6 text-xl">{queryComment.comment}</p>
+
+		<div class="p-5">
+			<p class="h2">{queryComment.reward}</p>
+			<span class="text-tertiary-900">Reward</span>
+		</div>
+	</div>
+
+	<div class="flex justify-between">
+		<span class="text-tertiary-900 pl-10">{created} by {queryComment.userId}</span>
 		{#if $authStore.isAuthenticated}
-			<button type="button" class="btn variant-filled" on:click={() => like(queryComment.hash)}>
-				LIKE
-			</button>
+			<div class="p-4">
+				<Button text="LIKE" on:click={() => like(queryComment.hash)} />
+			</div>
 		{/if}
-	</footer>
+	</div>
 </div>
+
+<!-- <div class="mr-3 ml-3 mt-5 mb-5 flex justify-between ">
+	<div>
+		<p class="h2">User{user.id}</p>
+		<span class="text-tertiary-900">Logged in</span>
+	</div>
+	<div>
+		<p class="h2">{user.balance}</p>
+		<span class="text-tertiary-900">Balance</span>
+	</div>
+</div> -->
 
 <style>
 </style>
